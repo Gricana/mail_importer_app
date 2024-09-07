fb5a5173f1cac901c9b668cab21580507bcd885b
Gricana <alex2904.goryachev@yandex.com>
2024-09-07 18:05:50 +0300

    Added logic for receiving emails and refactored

--- mails/views.py
+++ mails/views.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from .models import Email
+
 
 @login_required
 def mails(request):
-    return render(request, 'mail/mails.html')
+    """
+    This function retrieves and displays a list of emails for the logged-in user.
+    """
+    emails = Email.objects.filter(account=request.user)
+    return render(request, 'mail/mails.html', {'emails': emails})

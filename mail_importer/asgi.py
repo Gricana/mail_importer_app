2ddeb915daf2337f485d418289c9e6fa51a3dacf
Gricana <alex2904.goryachev@yandex.com>
2024-09-07 17:42:17 +0300

    Setting up a WS connection

--- mail_importer/asgi.py
+++ mail_importer/asgi.py
@@ -1,12 +1,16 @@
 import os
 
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+django.setup()
+
 import mails.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yourproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mail_importer.settings')
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),

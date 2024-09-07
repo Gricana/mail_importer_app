07ecc40895af5a3e56101c6e98ffc64cd55a7b43
Gricana <alex2904.goryachev@yandex.com>
2024-09-07 18:00:25 +0300

    The password storage mechanism has been changed and documentation has been added

--- account/models.py
+++ account/models.py
@@ -1,16 +1,22 @@
+from cryptography.fernet import Fernet
+from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
 from .managers import AccountManager
 
 
+cipher_suite = Fernet(settings.PASSWORD_KEY.encode())
+
+
 class Account(AbstractBaseUser):
     email = models.EmailField(verbose_name='Email', max_length=128, unique=True)
+    password = models.BinaryField()
     last_login = None
 
-    # Для "простоты" входа в админку
+    # For “simplicity” of logging into the admin panel
     is_staff = True
-    is_admin = True
+    is_active = True
     is_superuser = True
 
     objects = AccountManager()
@@ -20,12 +26,54 @@ class Account(AbstractBaseUser):
     def __str__(self):
         return self.email
 
+    def set_password(self, raw_password):
+        """
+        Encrypts the raw password using the cipher suite and sets it as the account's password.
+
+        Parameters:
+        raw_password (str): The raw password to be encrypted.
+
+        Returns:
+        None
+        """
+        password = cipher_suite.encrypt(raw_password.encode())
+        self.password = password
+
+    def get_password(self):
+        """
+        Decrypts the account's password and returns it as a string.
+
+        Returns:
+        str: The decrypted password.
+        """
+        decrypted_password = cipher_suite.decrypt(self.password).decode()
+        return decrypted_password
+
+    def check_password(self, raw_password):
+        """
+        Decrypts the account's password and checks if it matches the raw password.
+
+        Parameters:
+        raw_password (str): The raw password to be checked.
+
+        Returns:
+        bool: True if the raw password matches the decrypted password, False otherwise.
+        """
+        decrypted_password = cipher_suite.decrypt(self.password).decode()
+        return decrypted_password == raw_password
+
     def has_perm(self, perm, obj=None):
-        """Проверка наличия прав доступа"""
+        """
+        Checking for access rights
+        (Optional functionality!) To simplify the development process
+        """
         return True
 
     def has_module_perms(self, app_label):
-        """Проверка прав доступа к модулям"""
+        """
+        Checking access rights to modules.
+        (Optional functionality!) To simplify the development process
+        """
         return True
 
     class Meta:

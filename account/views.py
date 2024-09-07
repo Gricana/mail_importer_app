a0e76afb10e34ee244d798b7c2ed924418ebdb15
Gricana <alex2904.goryachev@yandex.com>
2024-09-07 18:05:11 +0300

    Documentated

--- account/views.py
+++ account/views.py
@@ -1,15 +1,21 @@
 from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import reverse
 
 from .forms import AccountAuthForm
 
 
 class AccountLoginView(LoginView):
+    """
+    A Django view for handling user login.
+
+    Attributes:
+    template_name: The name of the HTML template to render for the login page.
+    authentication_form: The form class to use for user authentication.
+    next_page: The URL name to redirect to after successful login.
+    """
+
     template_name = 'account/login.html'
     authentication_form = AccountAuthForm
-
-    def get_success_url(self):
-        return reverse('mails')
+    next_page = 'mails'
 
 
 class AccountLogoutView(LogoutView):
